0edaa96a8e7e18a939f23449b47e9c0545a9c91d
xopherw <xopher95@gmail.com>
2022-01-27 18:49:51 +1100

    fix divided by zero bug

--- algo.py
+++ algo.py
@@ -7,7 +7,7 @@ def trade(ticker, is_held, order):
     # Constants (change stock_divider to change the number of stocks to buy and add 1 more to the number of stocks)
     stock_divider = 3 - len(caller.stock_list(api))
     money = caller.money(api)/stock_divider
-    qty = int( money / data['close'].iloc[-1])
+    
 
     # Logics
     buy_logic_1 = (data['ema3'].iloc[-1] > data['ema10'].iloc[-1] and data['slope'].iloc[-1] > 0)
@@ -18,6 +18,7 @@ def trade(ticker, is_held, order):
     if(not(is_held) and (buy_logic_1 or buy_logic_2)):
         print("Buy")
         is_held = True
+        qty = int( money / data['close'].iloc[-1])
         order = not(order)
         caller.order(ticker, qty, order, api)
 
