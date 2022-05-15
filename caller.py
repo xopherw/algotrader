f60a2e58077a6d147405bf2dd4b404869fc1945f
xopherw <xopher95@gmail.com>
2022-05-15 22:15:16 +1000

    replace twelvedata

--- caller.py
+++ caller.py
@@ -1,27 +1,28 @@
 import requests, datetime as dt, numpy as np, pandas as pd, pytz
 from dateutil.relativedelta import relativedelta
 
-# Call for raw data (past data)
-def twelve_data(ticker, api):
+
+# Call for raw data (NASDAQ)
+def nsdq_data(ticker, api):
     try:
         today = dt.datetime.now(pytz.timezone('US/Eastern')).date()
+        past = today - relativedelta(years= 5)
         price = current_price(ticker.upper())
-        new_data = {"datetime" : today, "close" : price}
-        past = today - relativedelta(months = 12)
+        new_data = {"date" : today.strftime("%m/%d/%Y"), "close" : price}
+        headers = {'user-agent' : "-"}
+        url = "https://api.nasdaq.com/api"
+        post = f"/quote/{ticker.upper()}/historical"
         params = {
-            "symbol"        :   ticker.upper(),
-            "interval"      :   "1day",
-            "start_date"    :   str(past),
-            "end_date"      :   str(today),
-            "apikey"        :   api.twelve_data
+            "assetclass" : "stocks",
+            "fromdate"    :   past,
+            "limit"      :   '100000',
         }
-        # Retrieve
-        url = f"https://api.twelvedata.com/time_series"
-        r = requests.get(url, params=params).json()
-        
-        # Clean
-        data = pd.DataFrame(r['values'][::-1]).drop(columns=['volume', 'high', 'low', 'open'], axis=1) # Reverse and convert to DataFrame
-        data[['close']] = data[['close']].astype(float) # Convert 'close' to float type
+        r = requests.get(url + post, headers=headers, params=params).json()
+
+        # data cleaning and formatting
+        # Remove unnecessary data and reverse order
+        data = pd.DataFrame(r["data"]["tradesTable"]["rows"][::-1]).drop(columns=['volume', 'high', 'low', 'open'], axis=1) 
+        data[['close']] = data[['close']].replace('\$|,', '', regex=True).astype(float) # Convert 'close' to float type
         data = data.append(new_data, ignore_index=True) # Append latest data (aproaching closing time)
 
         # Calculate and add ema3, ema10, and slope to data
@@ -34,8 +35,46 @@ def twelve_data(ticker, api):
 
         return data
     except Exception as e:
-        print("12 Data Error:", e)
+        print("NSDQ Data Error: ", e)
         pass
+
+
+# Call for raw data (past data) 
+# NOTE: THIS CODE IS UNUSABLE PLEASE USE NASDAQ API.
+# def twelve_data(ticker, api):
+#     try:
+#         today = dt.datetime.now(pytz.timezone('US/Eastern')).date()
+#         price = current_price(ticker.upper())
+#         new_data = {"datetime" : today, "close" : price}
+#         past = today - relativedelta(months = 12)
+#         params = {
+#             "symbol"        :   ticker.upper(),
+#             "interval"      :   "1day",
+#             "start_date"    :   str(past),
+#             "end_date"      :   str(today),
+#             "apikey"        :   api.twelve_data
+#         }
+#         # Retrieve
+#         url = f"https://api.twelvedata.com/time_series"
+#         r = requests.get(url, params=params).json()
+        
+#         # Clean
+#         data = pd.DataFrame(r['values'][::-1]).drop(columns=['volume', 'high', 'low', 'open'], axis=1) # Reverse and convert to DataFrame
+#         data[['close']] = data[['close']].astype(float) # Convert 'close' to float type
+#         data = data.append(new_data, ignore_index=True) # Append latest data (aproaching closing time)
+
+#         # Calculate and add ema3, ema10, and slope to data
+#         ema3 = data['close'].ewm(span=3, adjust=False).mean() 
+#         ema10 = data['close'].ewm(span=10, adjust=False).mean() 
+#         slope= np.gradient(data['close']) 
+#         data['ema3'] = ema3 
+#         data['ema10'] = ema10 
+#         data['slope'] = slope
+
+#         return data
+#     except Exception as e:
+#         print("12 Data Error:", e)
+#         pass
 
 # Call for current price
 def current_price(ticker):
@@ -170,13 +209,12 @@ def next_open_time(api):
             "APCA-API-KEY-ID" : api.alpaca_api,
             "APCA-API-SECRET-KEY" : api.alpaca_secret,
         }
-        r = requests.get(url + post, headers=headers).json()['next_open']
-        next_open = dt.datetime.strptime(r[:-6],"%Y-%m-%dT%H:%M:%S")
+        r = requests.get(url + post, headers=headers).json()
+        next_open = dt.datetime.strptime(r['next_open'][:-6],"%Y-%m-%dT%H:%M:%S")
         return next_open
     except Exception as e:
         print("Next open time Error:", e)
         pass
-
 
 # Convert string date to date object
 def dateconvert(date):
