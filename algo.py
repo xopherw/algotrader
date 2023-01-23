37347814b4d06bbf9eb013851cf26f540e596152
xopherw <xopher95@gmail.com>
2023-01-23 21:20:43 +1100

    fix data calling

--- algo.py
+++ algo.py
@@ -11,7 +11,7 @@ def trade(ticker):
         stock_divider = len(ticker) + 1 - len(caller.stock_list(api))
         money = round(caller.money(api)/stock_divider,2)
         
-        print("ema 7: " + str(data['ema7'].iloc[-1]) + " | ema14: " + str(data['ema14'].iloc[-1]) + " | slope: " + str(data['slope'].iloc[-1]))
+        print("ema 7: " + str(data['ema7']) + " | ema14: " + str(data['ema14']) + " | slope: " + str(data['slope']))
 
         # # Logics (discontinue)
         # buy_logic_1 = (data['ema7'].iloc[-1] > data['ema10'].iloc[-1] and data['slope'].iloc[-1] > 0)
